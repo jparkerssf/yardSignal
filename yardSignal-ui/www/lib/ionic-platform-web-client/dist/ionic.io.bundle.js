9ef8255de229fa7af6e828bea3f7e1001b42c06c
jparkerssf <jparker@softstackfactory.org>
2016-09-27 19:59:35 +0000

    html setup

--- yardSignal-ui/www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
+++ yardSignal-ui/www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
@@ -3439,7 +3439,7 @@ var BaseSettings = (function () {
 })();
 
 var temp = new BaseSettings().factory('$ionicCoreSettings', function () {
-  "IONIC_SETTINGS_STRING_START";var settings = {"dev_push":true,"app_id":"60120bd7","api_key":"8c558a1004565c877ae80d8931e71cae0b9f024f4e3928b5"}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
+  "IONIC_SETTINGS_STRING_START";var settings = {"dev_push":true,"app_id":"ea734610","api_key":"f5675bb5ddee0bec6270288eb26edd4672f219b9e687c69c"}; return { get: function(setting) { if (settings[setting]) {return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
 }).finish();
 
 var Settings = (function () {
